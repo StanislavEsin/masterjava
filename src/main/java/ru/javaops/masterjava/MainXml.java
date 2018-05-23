042569877937a59977d1a6511e4bfb7cb1b76ea2
StanislavEsin <376825@gmail.com>
2018-05-23 16:06:58 +0600

    HW2 - create method "getSortedListUsersByNameProjectUsingStAX"

--- src/main/java/ru/javaops/masterjava/MainXml.java
+++ src/main/java/ru/javaops/masterjava/MainXml.java
@@ -1,16 +1,21 @@
 package ru.javaops.masterjava;
 
 import ru.javaops.masterjava.xml.schema_hw.*;
-import java.util.Comparator;
-import java.util.List;
-import java.util.ArrayList;
-import java.util.stream.Collectors;
-import java.util.Optional;
-import com.google.common.io.Resources;
 import ru.javaops.masterjava.xml.util.JaxbParser;
 import ru.javaops.masterjava.xml.util.Schemas;
-import javax.xml.bind.JAXBException;
+import ru.javaops.masterjava.xml.util.StaxStreamProcessor;
+import java.util.List;
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import com.google.common.io.Resources;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
+import javax.xml.stream.events.XMLEvent;
 import java.io.IOException;
+import javax.xml.bind.JAXBException;
 
 /**
  * MainXml.
@@ -25,8 +30,8 @@ public class MainXml {
             System.exit(1);
         }
 
-        List<User> users = new MainXml().getSortedListUsersByNameProjectUsingJAXB(args[0], args[1]);
-
+        List<User> usersJAXB = new MainXml().getSortedListUsersByNameProjectUsingJAXB(args[0], args[1]);
+        List<User> usersStAX = new MainXml().getSortedListUsersByNameProjectUsingStAX(args[0], args[1]);
     }
 
     public List<User> getSortedListUsersByNameProjectUsingJAXB(String projectName, String xmlName) {
@@ -53,5 +58,46 @@ public class MainXml {
         }
 
         return result;
+    }
+
+    public List<User> getSortedListUsersByNameProjectUsingStAX(String projectName, String xmlName) {
+        List<User> result = new ArrayList<>();
+
+        try (StaxStreamProcessor processor = new StaxStreamProcessor(Resources.getResource(xmlName).openStream())) {
+            XMLStreamReader reader = processor.getReader();
+
+            List<String> projectGroups = new ArrayList<>();
+
+            boolean pointerProject = false;
+            while (reader.hasNext()) {
+                int event = reader.next();
+                if (event == XMLEvent.START_ELEMENT) {
+                    if ("Project".equals(reader.getLocalName())) {
+                        pointerProject = projectName.equals(reader.getAttributeValue(0));
+                    }
+
+                    if ("Group".equals(reader.getLocalName()) && pointerProject) {
+                        projectGroups.add(reader.getAttributeValue(0));
+                    }
+
+                    if ("User".equals(reader.getLocalName())) {
+                        List<String> userGroups = Arrays.asList(reader.getAttributeValue(3).split(" "));
+
+                        if (userGroups.stream().anyMatch(projectGroups::contains)) {
+                            User user = new User();
+                            user.setEmail(reader.getAttributeValue(0));
+                            user.setValue(reader.getElementText());
+                            result.add(user);
+                        }
+                    }
+                }
+            }
+        } catch (XMLStreamException | IOException e) {
+            e.printStackTrace();
+        }
+
+        return result.stream()
+                .sorted(Comparator.comparing(User::getValue))
+                .collect(Collectors.toList());
     }
 }
